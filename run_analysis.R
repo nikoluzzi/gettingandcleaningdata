ddc822d7a842c6494e28437a917693b4ace6f0e5
Nicholas Mandelbaum <nicholas.mandelbaum@gmail.com>
2015-09-20 02:05:40 +0100

    first commit

---
+++ run_analysis.R
@@ -0,0 +1,68 @@
+library(dplyr)
+library(plyr)
+##library(tidyr)
+##Read the file
+xTestPath <- "./UCI HAR Dataset/test/X_test.txt"
+yTestPath <- "./UCI HAR Dataset/test/y_test.txt"
+subjectTestPath <- "./UCI HAR Dataset/test/subject_test.txt"
+
+xTrainPath <- "./UCI HAR Dataset/train/X_train.txt"
+yTrainPath <- "./UCI HAR Dataset/train/y_train.txt"
+subjectTrainPath <- "./UCI HAR Dataset/train/subject_train.txt"
+
+featuresPath <- "./UCI HAR Dataset/features.txt"
+activityPaths <- "./UCI HAR Dataset/activity_labels.txt"
+
+xTestData <- read.table(xTestPath)
+yTestData <- read.table(yTestPath,col.names = "Activity")
+subjectTestData <- read.table(subjectTestPath,col.names = "Subject")
+
+
+
+xTrainData <- read.table(xTrainPath)
+yTrainData <- read.table(yTrainPath,col.names = "Activity")
+subjectTrainData <- read.table(subjectTrainPath,col.names = "Subject")
+
+
+
+allTest <- bind_cols(xTestData,yTestData,subjectTestData)
+allTrain <- bind_cols(xTrainData,yTrainData,subjectTrainData)
+
+##Create the tbl that binds Test and Train data.
+allData <- bind_rows(allTest,allTrain)
+
+##Fetching the column index
+features <- read.table(featuresPath)
+
+##Filtering only what is mean or std. Taking meanFreq out of the set since it's the mean of Frequence measurement.
+filteredFeatures <- filter(features, grepl('mean[^Freq]|std',V2))
+
+
+##Create a vector to subset Alldata.
+## First add the variables 
+subsetVector <- filteredFeatures[,1]
+subsetNames <- as.character(filteredFeatures[,2])
+
+## Then append the Activity and Subject row number to the subset variable
+subsetVector <- c(subsetVector,562,563)
+
+##Subset the whole data, using the subsetVector, to keep only the columns wanted, including activity and subjects
+data <- allData[,subsetVector]
+
+##Fetch activity labels name
+activityLabels <- read.table(activityPaths, stringsAsFactors = FALSE)
+##Clean the names.
+activityLabels <- mapvalues(activityLabels$V2, from=activityLabels$V2, to = tolower(activityLabels$V2))
+
+## Apply the activity names
+data$Activity <- mapvalues(data$Activity, from = c(1, 2, 3,4,5,6), to = activityLabels)
+
+## Clean the columns names by 
+### 
+subsetNames <- tolower(subsetNames)
+subsetNames <- gsub("-","",subsetNames)
+subsetNames <- gsub("\\(\\)","",subsetNames)
+subsetNames <- c(subsetNames,"Activity","Subject")
+
+names(data) <- subsetNames
+data2 <- data2 %>% group_by(Activity, Subject) %>% summarise_each(c("mean"))
